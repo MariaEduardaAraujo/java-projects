3d7f3c017ecfe577827b6e9d30d4d5a766854a01
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-10-13 21:01:39 -0300

    Adicionando arquivo ao repositório

---
+++ Graduação/aula300924.java
@@ -0,0 +1,20 @@
+package Java;
+
+/*public class aula300924 {
+    public static void main(String[] args) {
+        System.out.println("Hello World em Java!");
+    }
+    
+}*/
+
+public class aula300924 {
+    public static void main(String[] args) {
+        int var1, var2;
+
+        var1 = 1024;
+        var2 = var1/2;
+
+        System.out.println("Var1 é: " + var1); 
+        System.out.println("Var2 é: " + var2); //println - quebra linha
+    }
+}
