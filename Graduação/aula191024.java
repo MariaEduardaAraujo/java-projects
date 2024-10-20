934ad1466c28e391af8f9affcdef48d289ec8ceb
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-10-20 20:49:53 -0300

    Resolvendo exercício de Java

--- Graduação/aula191024.java
+++ Graduação/aula191024.java
@@ -1,4 +1,37 @@
-/*3) Calcule e mostre a média dos números pares entre 1 e 100, inclusive.*/
+import java.util.Scanner;
 
+/*3) Calcule e mostre a média dos números pares entre 1 e 100, inclusive.
+
+public class aula191024 {
+    public static void main(String args[]){
+        int media = 0, contPares = 0, soma = 0;
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0) {
+                contPares++;
+                soma = soma + i;
+            }
+        }
+        media = soma/contPares;
+        System.out.println("A média dos números pares entre 1 e 100 é: " + media);
+    }
+}*/
 
 /*4) Leia 2 valores: X e Y. A seguir, calcule e mostre a soma dos números impares entre eles.*/
+
+public class aula191024 {
+    public static void main(String[] args) {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um valor: ");
+        x = sc.nextInt();
+        System.out.println("Digite um outro valor: ");
+        y = sc.nextInt();
+
+        if (x % 2 != 0 || y % 2 != 0) {
+            
+        }
+        
+        sc.close();
+    }
+}
