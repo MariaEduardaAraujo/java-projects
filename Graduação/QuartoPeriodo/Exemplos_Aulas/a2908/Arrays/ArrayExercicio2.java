472432a7633f312d6253045c0ac7023686f6f4ee
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2025-09-08 10:06:56 -0300

    Complete activity a2908

--- Graduação/QuartoPeriodo/Exemplos_Aulas/a2908/Arrays/ArrayExercicio2.java
+++ Graduação/QuartoPeriodo/Exemplos_Aulas/a2908/Arrays/ArrayExercicio2.java
@@ -1,4 +1,4 @@
-package aula2908.Arrays;
+package a2908.Arrays;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -11,16 +11,38 @@ public class ArrayExercicio2 {
         
         Random rd = new Random();
         int numAleatorio = rd.nextInt(100)+1;
-        int chute[] = new int[100];
+        int chances = 5;
+        int chute[] = new int[chances];
+        
+        System.out.println(numAleatorio);
 
         for (int i = 0; i < chute.length; i++){
             System.out.print("Digite seu palpite: ");
             int palp = sc.nextInt();
-            chute[i] = palp;
             
-            if (chute) {
-                
+            boolean repetido = false;
+            for (int j = 0; j < i; j++) {  // só até i, porque o resto ainda não foi preenchido
+                if (palp == chute[j]) {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            if (repetido) {
+                System.out.print("Você já deu esse palpite! ");
+                i--;
+            }else{
+                chute[i] = palp;
+                System.out.print("Palpite registrado! ");
+            }
+
+            if (palp == numAleatorio) {
+                System.out.println("Acertou!");
+                break;
+            }else{
+                System.out.println("\nMas você não acertou! Tente novamente!");
             }
         }
-        
+        sc.close();
+    }
 }
